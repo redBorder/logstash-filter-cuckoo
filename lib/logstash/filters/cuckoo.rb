dedb74cb20af18edbb97591f48e57b9dc78207cb
manegron@redborder.com <manegron@redborder.com>
2022-12-15 19:29:40 +0000

    Refactor!

--- lib/logstash/filters/cuckoo.rb
+++ lib/logstash/filters/cuckoo.rb
@@ -7,100 +7,94 @@ require 'json'
 require 'rest-client'
 require 'digest'
 
-require 'mime/types'
-# require 'filemagic'
-
 class LogStash::Filters::Cuckoo < LogStash::Filters::Base
 
   config_name "cuckoo"
 
   # Hostname where cuckoo_api is running
-  config :host,                           :validate => :string,   :default => "localhost:8090"
-  config :file_path,                      :validate => :string,   :required => true
-  
-  # Config file where are set cuckoo excluded mime types
-  config :config_file,                        :validate => :string,   :default => "/opt/rb/var/rb-sequence-oozie/conf/config.yml"
+  config :cuckoo_server,                            :validate => :string,   :default => "localhost:8090"
+  config :cuckoo_exclude_file_types,                :validate => :array,    :default => []
+  config :cuckoo_file,                              :validate => :string,   :default => "[path]"
+
+  config :file_bin_path,                            :validate => :string,   :default => "/usr/bin/file"
+  config :tag_on_create_cuckoo_task_failure,        :validate => :array,    :default => ['_createcuckootaskfailure'] 
 
   public
   def register
-
   end # def register
 
   private
 
-  # # Check file mime type
-  # # @return boolean whether file mime type is excluded for analysis or not
-  # def filetype_is_excluded?(file_mime_type)
-  #   begin
-  #     filters = YAML.load_file(@config_file)["cuckoo"]["cuckoo_filters"]
-  #     filters.include? file_mime_type
-  #   rescue Errno::ENOENT => e
-  #     nil
-  #   end
-  # end
+  # Send file to be analyzed by cuckoo.
+  # @return returns cuckoo analysis task id.
+  def create_cuckoo_task(file_path)
+    task_id = nil
+ 
+    begin
+      file_name = ::File.basename(file_path)
+      file =      ::File.open(file_path, 'r')
+      options = {filename: file_name, file: file}
+    rescue Errno::ENOENT=> ex
+      @logger.error(ex.message)
+      return task_id
+    rescue Errno::EACCES=> ex
+      @logger.error(ex.message)
+      return task_id
+    end
+ 
+    begin
+      response = RestClient::Request.execute(
+        method: "post",
+        url: "http://#{@cuckoo_server}/tasks/create/file",
+        payload: options,
+        timeout: 120
+      )
+    rescue RestClient::Exception => ex
+      @logger.error(ex.message)
+      return task_id
+    end
+    JSON.parse(response.body)["task_ids"] rescue nil 
+  end 
 
-  # # Send file to be analyzed by cuckoo.
-  # # @return returns cuckoo analysis task id.
-  # def send_file
-  #   @logger.info("Sending file to be analyzed.")
 
-  #   task_id = nil
-
-  #   begin
-  #     file_name = ::File.basename(@path)
-  #     file =      ::File.open(@path, 'r')
-  #     options = {filename: file_name, file: file}
-  #   rescue Errno::ENOENT=> ex
-  #     @logger.error(ex.message)
-  #     return task_id
-  #   rescue Errno::EACCES=> ex
-  #     @logger.error(ex.message)
-  #     return task_id
-  #   end
-
-
-  #   begin
-  #     response = RestClient::Request.execute(
-  #       method: "post",
-  #       url: "http://#{@host}/tasks/create/file",
-  #       payload: options
-  #     )
-  #   rescue RestClient::Exception => ex
-  #     @logger.error(ex.message)
-  #     return task_id
-  #   end
-
-  #   JSON.parse(response.body)["task_ids"]
-
-  # end
-
+  def detect_mime_type(file_path)
+    # if the user has installed file binary, we can use that
+    # if not, return nil
+    return nil unless File.exist?(@file_bin_path)
+  
+    # Attempt to detect the MIME type using the file binary
+    type, _ = `#{@file_bin_path} --mime -b #{file_path}`.split('; ') rescue nil
+  
+    # Return the MIME type as a string, or nil if it could not be detected
+    type ? type.to_s : nil
+  end
+  
   public
   def filter(event)
-    @logger.info("[Cuckoo] processing #{@file_path}")
-
     begin
-      return unless File.exists?(filename)
-      mime_type = MIME::Types.type_for(filename).first
+      @logger.info("[Cuckoo] Checking event #{event}")
 
-      @logger.info("File #{@file_path)} mime type is #{mime_type}")
+      cuckoo_file_path = event.get(@cuckoo_file)
 
-      excluded = filetype_is_excluded?(mime_type)
+      unless cuckoo_file_path and File.exist?cuckoo_file_path and !@cuckoo_exclude_file_types.include? detect_mime_type(cuckoo_file_path)
+         @logger.info("[Cuckoo] Nothing to do.")
+         return
+      end
 
-      # if excluded
-      #   @logger.info("Mime type #{mime_type} is excluded from Cuckoo Analysis")
-      # elsif excluded.nil?
-      #   @logger.error("Error parsing file #{@config_file}.")
-      # else
-      #   task_id = send_file
-      #   if task_id
-      #     @logger.info("Task #{task_id} has been created.")
-      #   else
-      #     @logger.info("There was an error creating Cuckoo task. Check filter configuration and cuckoo_api service status.")
-      #   end
-      # end
+      @logger.info("[Cuckoo] Sending file to be analyzed.")
+      cuckoo_task_id = create_cuckoo_task(cuckoo_file_path)
+      
+      if cuckoo_task_id
+        event.set("cuckoo_task_id", cuckoo_task_id) if cuckoo_task_id
+      else
+        @tag_on_create_cuckoo_task_failure.each { |tag| event.tag(tag) }
+      end
+
     rescue => e
       @logger.error(e.message)
     end
+
+    @logger.info("[Cuckoo] output event: #{event}")
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
